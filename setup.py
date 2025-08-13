fef4f402bc86ab09bd01cf7b449ab1ae86e1c84a
Jitesh <jiteshvk13@gmail.com>
2025-08-13 13:29:05 +0530

    My First Commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN_E_DOT='-e .'
+
+def get_requirements(file_path:str) ->List[str]:
+    '''
+    This Function Will Return the List of Requirments
+    '''
+    requirements = []
+
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirments = [req.replace('\n', "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+
+    return requirements
+
+
+setup(
+name = "mlproject",
+version = "0.0.1",
+author = "Jitesh",
+author_email= "jiteshvk13@gmail.com",
+packages= find_packages(),
+install_requires= get_requirements("requirements.txt")
+
+)
